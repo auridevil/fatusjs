fc18d16afe19345288db1b9576bd2e6d3992e9ab
Aureliano <aureliano@digitalx.it>
2016-03-22 09:27:45 +0100

    base worker

--- worker.js
+++ worker.js
@@ -11,6 +11,7 @@ const MODULE_NAME = 'FatusWorker';
 const shortid = require('shortid');
 const EventEmitter = require('events');
 const util = require('util');
+const path = require('path');
 
 class FatusWorker extends EventEmitter{
 
@@ -22,7 +23,45 @@ class FatusWorker extends EventEmitter{
         //util.inherits(this(),EventEmitter);
     }
 
-    
+
+    getPath(inputPath){
+        let normalizedPath = path.normalize(global.__base + inputPath);
+        return normalizedPath;
+    }
+
+    getModule(moduleName){
+        return require(this.getPath(moduleName));
+    }
+
+    execute(moduleName,funct,payload,onComplete){
+        try{
+            let module = getModule(moduleName);
+            module[funct](payload,this,onComplete);
+        }catch(exception){
+            // manage exception
+        }
+    }
+
+    run(){
+        let th = this;
+        async.waterfall([
+            
+            function top(wfcallback){
+                th.fatus.getQueueTop(wfcallback);
+            },
+
+            function msgExecute(msg,wfcallback){
+                th.execute(msg.messageText.module,msg.messageText.function,JSON.parse(msg.messageText.payload),wfcallback);
+            }
+
+            // continue from here
+        ]
+    }
+
+
+
+
+
 
 }
 
